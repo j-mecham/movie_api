94d8d11ce9c88f2ed60d0a964f0bad66e1839f7b
j-mecham <josh.mecham@gmail.com>
2023-07-12 17:51:26 -0600

    connected to online hosted DB

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ let auth = require('./auth.js')(app);
 
 const Movies = Models.Movie;
 const Users = Models.User;
-mongoose.connect('mongodb://127.0.0.1:27017/filmFanDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://127.0.0.1:27017/filmFanDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(morgan('common'));
 app.use(express.static('public'));
